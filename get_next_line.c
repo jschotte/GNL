16367903a966c120bfc15ca43cc9cfd0af71ae92
Jeremy SCHOTTE <jschotte@e3r2p4.42.fr>
2016-01-06 13:31:17 +0100

    soon

--- get_next_line.c
+++ get_next_line.c
@@ -6,14 +6,13 @@
 /*   By: jschotte <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/12/28 09:30:10 by jschotte          #+#    #+#             */
-/*   Updated: 2016/01/04 16:28:35 by jschotte         ###   ########.fr       */
+/*   Updated: 2016/01/06 13:23:54 by jschotte         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
-#include <stdio.h>
 
-static int	get_line(int const fd, char *buffer, char *save[fd], int *end)
+static int	get_line(int const fd, char *buffer, char *save[fd])
 {
 	int		ret;
 	char	*c;
@@ -27,8 +26,6 @@ static int	get_line(int const fd, char *buffer, char *save[fd], int *end)
 		save[fd] = ft_strjoin(tmp, buffer);
 		ft_strdel(&tmp);
 	}
-	if (buffer == NULL && ret == 0)
-		*end = 1;
 	ft_strdel(&buffer);
 	if (ret == -1)
 		return (-1);
@@ -37,26 +34,12 @@ static int	get_line(int const fd, char *buffer, char *save[fd], int *end)
 	return (1);
 }
 
-static int	get_endfile(int *end, char **save, char **buffer, char **line)
-{
-	if (ft_strlen(*buffer) != 0 && *end == 0)
-	{
-		*line = ft_strdup(*buffer);
-		*save = NULL;
-		*end = 1;
-		return (1);
-	}
-	*line = *save;
-	*save = NULL;
-	return (0);
-}
-
 int			get_next_line(int const fd, char **line)
 {
 	static char	*save[MAX_FD];
 	char		*buffer;
 	int			ret;
-	static int	end = 0;
+	char		*str;
 	char		*tmp;
 
 	buffer = ft_strnew(BUFF_SIZE);
@@ -64,16 +47,27 @@ int			get_next_line(int const fd, char **line)
 		return (-1);
 	if (save[fd] == NULL)
 		save[fd] = ft_strnew(1);
-	ret = get_line(fd, buffer, save, &end);
+	ret = get_line(fd, buffer, save);
 	if (ret == -1)
 		return (-1);
-	if (ret == 0)
+	if ((str = ft_strchr(save[fd], '\n')) != NULL)
 	{
-		return (get_endfile(&end, &save[fd], &buffer, line));
+		*line = ft_strsub(save[fd], 0, str - save[fd]);
+		tmp = save[fd];
+		save[fd] = ft_strdup(str + 1);
+		ft_strdel(&tmp);
+		return (1);
 	}
-	*line = ft_strsub(save[fd], 0, ft_strchr(save[fd], '\n') - save[fd]);
-	tmp = save[fd];
-	save[fd] = ft_strdup(ft_strchr(save[fd], '\n') + 1);
-	ft_strdel(&tmp);
-	return (1);
+	while ((ret = read(fd, buffer, BUFF_SIZE)) > 0)
+	{
+		buffer[ret] = '\0';
+		tmp = save[fd];
+		save[fd] = ft_strjoin(tmp, buffer);
+		ft_strdel(&tmp);
+	}
+	*line = ft_strdup(save[fd]);
+	save[fd] = NULL;
+	if (ft_strlen(*line) > 0)
+		return (1);
+	return (0);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,14 +6,14 @@
 /*   By: jschotte <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/12/28 09:28:55 by jschotte          #+#    #+#             */
-/*   Updated: 2016/01/05 11:44:20 by jschotte         ###   ########.fr       */
+/*   Updated: 2016/01/06 13:28:47 by jschotte         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-# define BUFF_SIZE 50000
+# define BUFF_SIZE 1000
 # define MAX_FD 256
 # include <stdlib.h>
 # include <unistd.h>
